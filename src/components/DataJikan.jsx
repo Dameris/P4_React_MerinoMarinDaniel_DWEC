def5e0cb38b0f9ef4fe71172942e0ec606788e3f
Dameris <116887141+Dameris@users.noreply.github.com>
2024-01-12 14:26:19 +0100

    Add files via upload

--- src/components/DataJikan.jsx
+++ src/components/DataJikan.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { Card, Button } from "react-bootstrap"
+import { Card } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 const DataJikan = ({ search }) => {
