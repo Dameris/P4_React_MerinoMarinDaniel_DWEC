b347b5186577f6ee66249d6c3f057781b937b1e5
Daniel Merino <danimerinoo02@gmail.com>
2024-03-03 16:28:36 +0100

    03/03/2024

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,83 +3,174 @@ import { Navigate } from "react-router"
 import logo from "../images/logo.png"
 
 export default function SignUp() {
-  const [user, setUser] = useState({
-    email: "",
-    username: "",
-    password: "",
-    birthday: ""
-  })
+	const [user, setUser] = useState({
+		email: "",
+		username: "",
+		password: "",
+		birthday: "",
+	})
 
-  const [formErrors, setFormErrors] = useState({
-    emailError: false,
-    usernameError: false,
-    passwordError: false,
-    birthdayError: false
-  })
+	const [formErrors, setFormErrors] = useState({
+		emailError: false,
+		usernameError: false,
+		passwordError: false,
+	})
 
-  const [redirect, setRedirect] = useState(false)
+	const [redirect, setRedirect] = useState(false)
 
-  // Manejo de errores en el formulario
-  const validateEmail = () => {
-    const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/
-    setFormErrors({ ...formErrors, emailError: !emailPattern.test(formData.email) })
-  }
+	// Manejo de errores en el formulario
+	const validateEmail = () => {
+		const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/
+		setFormErrors({ ...formErrors, emailError: !emailPattern.test(user.email) })
+	}
 
-  // Manejar cambios en el formulario
-  const handleChange = (e) => {
-    const { name, value } = e.target
-    setUser({ ...user, [name]: value })
-  }
+	const validateUsername = () => {
+		const namePattern = /^[A-Za-zÀ-ÿ\s]{2,}$/
+		setFormErrors({ ...formErrors, usernameError: !namePattern.test(user.username) })
+	}
 
-  // Manejar los datos enviados
-  const handleSubmit = (e) => {
-    e.preventDefault()
+	const validatePassword = () => {
+		const lengthRegex = /.{8,}/
+		const upperCaseRegex = /[A-Z]/
+		const specialCharRegex = /[*, +, -, ., @, #, %, &, _, ~, ^, /, <, >]/
 
-    const storedUsers = localStorage.getItem("users")
-    const users = storedUsers ? JSON.parse(storedUsers) : []
-    const newUser = { ...user }
-    const userExists = users.some((existingUser) => existingUser.username === newUser.username);
-    const emailExist = users.some((existingEmail) => existingEmail.email === newUser.email);
+		const hasLength = lengthRegex.test(user.password)
+		const hasUpperCase = upperCaseRegex.test(user.password)
+		const hasSpecialChar = specialCharRegex.test(user.password)
 
-    if (userExists) {
-      alert("Username already exists")
-    } else if (emailExist) {
-      alert("Email already exists")
-    } else {
-      users.push(newUser)
-      localStorage.setItem("users", JSON.stringify(users))
+		setFormErrors({ ...formErrors, passwordError: !hasLength || !hasUpperCase || !hasSpecialChar })
+	}
 
-      setRedirect(true)
-    }
-  }
+	// Manejar cambios en el formulario
+	const handleChange = (e) => {
+		const { name, value } = e.target
+		setUser({ ...user, [name]: value })
+	}
 
-  if (redirect) {
-    return <Navigate to="/logIn" />
-  }
+	// Manejar los datos enviados
+	const handleSubmit = (e) => {
+		e.preventDefault()
 
-  return (
-    <div className="signUpWallpaper">
-      <img className="mainLogo" src={logo} alt="Main Logo" />
-      <form onSubmit={handleSubmit} className="formSignUp">
-        <div className="signUpBlock">
-          <label htmlFor="email">
-            <input className="signup" type="email" id="email" name="email" placeholder="Email" value={user.email} onChange={handleChange} required />
-          </label>
-          <label htmlFor="user">
-            <input className="signup" type="text" id="user" name="username" placeholder="User" minLength={4} maxLength={20} value={user.username} onChange={handleChange} required />
-          </label>
-          <label htmlFor="password">
-            <input className="signup" type="password" id="password" name="password" placeholder="Password" minLength={8} maxLength={20} value={user.password} onChange={handleChange} required />
-          </label>
-          <label htmlFor="birthday">
-            <input className="signup" type="date" id="birthday" name="birthday" value={user.birthday} onChange={handleChange} required />
-          </label>
-          <label htmlFor="terms">
-            <input type="checkbox" id="terms" name="terms" required /> I agree terms and conditions
-          </label>
-          <button className="submitSignUp" type="submit">Sign Up</button>
-        </div>
-      </form>
-    </div>
-  )
+		validateEmail()
+		validateUsername()
+		validatePassword()
+
+		// Comprobar si hay errores
+		if (formErrors.emailError || formErrors.usernameError || formErrors.passwordError) {
+			alert("All input fields must contain valid information.")
+		} else {
+			alert("Form submitted successfully!")
+		}
+
+		const storedUsers = localStorage.getItem("users")
+		const users = storedUsers ? JSON.parse(storedUsers) : []
+		const newUser = { ...user }
+		const userExists = users.some((existingUser) => existingUser.username === newUser.username)
+		const emailExist = users.some((existingEmail) => existingEmail.email === newUser.email)
+
+		if (userExists) {
+			alert("Username already exists")
+		} else if (emailExist) {
+			alert("Email already exists")
+		} else {
+			users.push(newUser)
+			localStorage.setItem("users", JSON.stringify(users))
+
+			setRedirect(true)
+		}
+	}
+
+	if (redirect) {
+		return <Navigate to="/logIn" />
+	}
+
+	return (
+		<div className="signUpWallpaper">
+			<img
+				className="mainLogo"
+				src={logo}
+				alt="Main Logo"
+			/>
+			<form
+				onSubmit={handleSubmit}
+				className="formSignUp"
+			>
+				<div className="signUpBlock">
+					<label htmlFor="email">
+						<input
+							className="signup"
+							type="email"
+							id="email"
+							name="email"
+							placeholder="Email"
+							value={user.email}
+							onChange={handleChange}
+							onBlur={validateEmail}
+							required
+						/>
+					</label>
+					{formErrors.emailError && <span>Please enter a valid email address</span>}
+
+					<label htmlFor="user">
+						<input
+							className="signup"
+							type="text"
+							id="user"
+							name="username"
+							placeholder="User"
+							value={user.username}
+							onChange={handleChange}
+							onBlur={validateUsername}
+							required
+						/>
+					</label>
+					{formErrors.usernameError && <span>Please enter a valid username</span>}
+
+					<label htmlFor="password">
+						<input
+							className="signup"
+							type="password"
+							id="password"
+							name="password"
+							placeholder="Password"
+							value={user.password}
+							onChange={handleChange}
+							onBlur={validatePassword}
+							required
+						/>
+					</label>
+					{formErrors.passwordError && <span>Please enter a valid password</span>}
+
+					<label htmlFor="birthday">
+						<input
+							className="signup"
+							type="date"
+							id="birthday"
+							name="birthday"
+							value={user.birthday}
+							onChange={handleChange}
+							required
+						/>
+					</label>
+
+					<label htmlFor="terms">
+						<input
+							type="checkbox"
+							id="terms"
+							name="terms"
+							required
+						/>{" "}
+						I agree terms and conditions
+					</label>
+
+					<button
+						className="submitSignUp"
+						type="submit"
+					>
+						Sign Up
+					</button>
+				</div>
+			</form>
+		</div>
+	)
 }
