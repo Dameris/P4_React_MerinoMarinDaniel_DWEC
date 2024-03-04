942233c0bcd083a049e0819de584e26d34e2846a
Daniel Merino <dmermar2302@g.educaand.es>
2024-03-04 12:29:27 +0100

    04/03/2024

--- src/components/AnimeDetails.jsx
+++ src/components/AnimeDetails.jsx
@@ -3,80 +3,86 @@ import { useParams, useNavigate } from "react-router-dom"
 import { useUserContext } from "../context/UserContext"
 
 const AnimeDetails = () => {
-    const { animeId } = useParams()
-    const [animeResults, setAnimeResults] = useState({ data: {} })
-    const [animeImg, setAnimeImg] = useState("")
-    const { logged, user, updateUserFavorites } = useUserContext()
-    const navigate = useNavigate()
+	const { animeId } = useParams()
+	const [animeResults, setAnimeResults] = useState({ data: {} })
+	const [animeImg, setAnimeImg] = useState("")
+	const { logged, user, updateUserFavorites } = useUserContext()
+	const navigate = useNavigate()
 
-    // Llamada a "Jikan API" para obtener la info completa de los animmes a partir del ID
-    useEffect(() => {
-        const getAnimeData = async () => {
-            try {
-                const data = await fetch(`https://api.jikan.moe/v4/anime/${animeId}/full`)
-                const result = await data.json()
-                setAnimeResults(result)
-                setAnimeImg(result.data.images?.jpg?.large_image_url)
-            } catch (error) {
-                console.error('Error fetching anime data from Jikan API', error)
-            }
-        }
+	// Llamada a "Jikan API" para obtener la info completa de los animmes a partir del ID
+	useEffect(() => {
+		const getAnimeData = async () => {
+			try {
+				const data = await fetch(`https://api.jikan.moe/v4/anime/${animeId}/full`)
+				const result = await data.json()
+				setAnimeResults(result)
+				setAnimeImg(result.data.images?.jpg?.large_image_url)
+			} catch (error) {
+				console.error("Error fetching anime data from Jikan API", error)
+			}
+		}
 
-        getAnimeData()
-    }, [animeId])
+		getAnimeData()
+	}, [animeId])
 
-    useEffect(() => {
-        const savedFavorites = user && user.favorites ? user.favorites : []
-        updateUserFavorites(savedFavorites)
-    }, [user, updateUserFavorites])
+	useEffect(() => {
+		const savedFavorites = user && user.favorites ? user.favorites : []
+		updateUserFavorites(savedFavorites)
+	}, [user, updateUserFavorites])
 
-    const addToFavorites = (animeId) => {
-        if (!user?.favorites.includes(animeId)) {
-            updateUserFavorites([...user.favorites, animeId])
-        }
-    }
+	const addToFavorites = (animeId) => {
+		if (!user?.favorites.includes(animeId)) {
+			updateUserFavorites([...user.favorites, animeId])
+		}
+	}
 
-    const removeFromFavorites = (animeId) => {
-        updateUserFavorites(user?.favorites.filter((id) => id !== animeId))
-    }
+	const removeFromFavorites = (animeId) => {
+		updateUserFavorites(user?.favorites.filter((id) => id !== animeId))
+	}
 
-    const anime = animeResults.data
+	const anime = animeResults.data
 
-    if (!anime) {
-        return <div>No anime found</div>
-    }
+	if (!anime) {
+		return <div>No anime found</div>
+	}
 
-    return (
-        <div className="detailsDiv">
-            <h1 className="text-center">{anime.title}</h1>
-            <h2 className="text-center">({anime.title_japanese})</h2>
-            <div className="imgSynopsis">
-                {animeImg && (
-                    <img src={animeImg} alt={anime.title} />
-                )}
-                {anime.synopsis}
-            </div>
+	return (
+		<div className="detailsDiv">
+			<h1 className="text-center">{anime.title}</h1>
+			<h2 className="text-center">({anime.title_japanese})</h2>
+			<div className="imgSynopsis">
+				{animeImg && (
+					<img
+						src={animeImg}
+						alt={anime.title}
+					/>
+				)}
+				{anime.synopsis}
+			</div>
 
-            <p className="detailsP">
-                <button onClick={() => {
-                    if (!logged) {
-                        navigate("/login")
-                    } else {
-                        if (user && user.favorites) {
-                            const isFavorite = user.favorites.includes(anime.mal_id)
-                            isFavorite ? removeFromFavorites(anime.mal_id) : addToFavorites(anime.mal_id)
-                        }
-                    }
-                }}>
-                    {user && user.favorites && user.favorites.includes(anime.mal_id) ? "Remove from favorites" : "Add to favorites"}
-                </button> <br />
-                Type: {anime.type} <br />
-                Source: {anime.source} <br />
-                Episodes: {anime.episodes} <br />
-                Status: {anime.status}
-            </p>
-        </div>
-    )
+			<p className="detailsP">
+				<button
+					onClick={() => {
+						if (!logged) {
+							navigate("/login")
+						} else if (user && user.favorites) {
+							const isFavorite = user.favorites.includes(anime.mal_id)
+							isFavorite ? removeFromFavorites(anime.mal_id) : addToFavorites(anime.mal_id)
+						}
+					}}
+				>
+					{user && user.favorites && user.favorites.includes(anime.mal_id)
+						? "Remove from favorites"
+						: "Add to favorites"}
+				</button>
+				<br />
+				Type: {anime.type} <br />
+				Source: {anime.source} <br />
+				Episodes: {anime.episodes} <br />
+				Status: {anime.status}
+			</p>
+		</div>
+	)
 }
 
 export default AnimeDetails
--- src/components/DataJikan.jsx
+++ src/components/DataJikan.jsx
@@ -3,59 +3,66 @@ import { Card } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 const DataJikan = ({ search, genre, page, resultsPerPage }) => {
-    const [animeResults, setAnimeResults] = useState([])
-    const [totalPages, setTotalPages] = useState(0)
+	const [animeResults, setAnimeResults] = useState([])
+	const [totalPages, setTotalPages] = useState(0)
 
-    // Llamada a "API Jikan" para obtener los animes
-    useEffect(() => {
-        const getAnimeData = async () => {
-            try {
-                const data = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&sfw&page=${page}&per_page=${resultsPerPage}`)
-                const result = await data.json()
-                setAnimeResults(result.data)
-                setTotalPages(result.pagination.items.total)
-            } catch (error) {
-                console.error("Error fetching anime data from Jikan API", error)
-            }
-        }
+	// Llamada a "API Jikan" para obtener los animes
+	useEffect(() => {
+		const getAnimeData = async () => {
+			try {
+				const data = await fetch(
+					`https://api.jikan.moe/v4/anime?q=${search}&sfw&page=${page}&per_page=${resultsPerPage}`
+				)
+				const result = await data.json()
+				setAnimeResults(result.data)
+				setTotalPages(result.pagination.items.total)
+			} catch (error) {
+				console.error("Error fetching anime data from Jikan API", error)
+			}
+		}
 
-        // Añadir un retraso de 1 segundo entre solicitudes
-        const timeoutId = setTimeout(() => {
-            getAnimeData()
-        }, 1000)
+		// Añadir un retraso de 1 segundo entre solicitudes
+		const timeoutId = setTimeout(() => {
+			getAnimeData()
+		}, 1000)
 
-        // Limpiar el temporizador para evitar problemas
-        return () => clearTimeout(timeoutId)
-    }, [search, genre, page, resultsPerPage])
+		// Limpiar el temporizador para evitar problemas
+		return () => clearTimeout(timeoutId)
+	}, [search, genre, page, resultsPerPage])
 
-    // Filtrar animes por género
-    const filteredAnimeResults = animeResults ? animeResults.filter((anime) => {
-        if (!genre) {
-            return true
-        } else {
-            return anime.genres.some((animeGenre) => genre === animeGenre.name)
-        }
-    }) : []
+	// Filtrar animes por género
+	const filteredAnimeResults = animeResults
+		? animeResults.filter((anime) => {
+				if (!genre) {
+					return true
+				} else {
+					return anime.genres.some((animeGenre) => genre === animeGenre.name)
+				}
+			})
+		: []
 
-    const handleClick = (mal_id) => {
-        alert(`Redirecting to anime with ID ${mal_id}`)
-    }
-
-    return (
-        <div>
-            <ul className="animeList">
-                {filteredAnimeResults.map((anime) => (
-                    <Card key={anime.mal_id} className="animeCard">
-                        <Card.Img variant="top" src={anime.images.jpg.image_url} className="animeImg" />
-                        <Card.Body></Card.Body>
-                        <Card.Footer className="cardName">
-                            <Link to={`/animeDetails/${anime.mal_id}`} onClick={() => handleClick(anime.mal_id)}>{anime.title}</Link>
-                        </Card.Footer>
-                    </Card>
-                ))}
-            </ul>
-        </div>
-    )
+	return (
+		<div>
+			<ul className="animeList">
+				{filteredAnimeResults.map((anime) => (
+					<Card
+						key={anime.mal_id}
+						className="animeCard"
+					>
+						<Card.Img
+							variant="top"
+							src={anime.images.jpg.image_url}
+							className="animeImg"
+						/>
+						<Card.Body></Card.Body>
+						<Card.Footer className="cardName">
+							<Link to={`/animeDetails/${anime.mal_id}`}>{anime.title}</Link>
+						</Card.Footer>
+					</Card>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default DataJikan
